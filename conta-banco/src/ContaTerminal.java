452017998210c2f2f07d17d1014361ce1180bbce
Gustavo Henrique <gustavo.toway@gmail.com>
2024-08-20 15:36:35 -0400

    meu projeto conta bancária

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,4 +2,11 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+    /**
+     * InnerContaTerminal
+     */
+    public interface InnerContaTerminal {
+    
+        
+    }
 }
