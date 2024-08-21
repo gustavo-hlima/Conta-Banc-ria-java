5bfd4e16a52662dc0b0742935a1403fdb1d675be
Gustavo Henrique <gustavo.toway@gmail.com>
2024-08-21 16:18:43 -0400

    Banco de dados

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -3,34 +3,31 @@ import java.util.Scanner;
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        // Criação de um objeto Scanner para capturar a entrada do usuário
+        // Criação do Scanner para capturar a entrada do usuário
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitação do número da conta
+        // Solicitação dos dados ao usuário
         System.out.println("Por favor, digite o número da Conta:");
-        int numero = scanner.nextInt(); // Lê um número inteiro para a conta
+        int numero = scanner.nextInt();
 
-        // Solicitação do número da agência
         System.out.println("Por favor, digite o número da Agência:");
-        scanner.nextLine(); // Limpa o buffer
-        String agencia = scanner.nextLine(); // Lê uma string para a agência
+        scanner.nextLine(); // Consumir a quebra de linha após o nextInt()
+        String agencia = scanner.nextLine();
 
-        // Solicitação do nome do cliente
         System.out.println("Por favor, digite o nome do Cliente:");
-        String nomeCliente = scanner.nextLine(); // Lê uma string para o nome do cliente
+        String nomeCliente = scanner.nextLine();
 
-        // Solicitação do saldo da conta
         System.out.println("Por favor, digite o saldo da Conta:");
-        double saldo = scanner.nextDouble(); // Lê um valor decimal para o saldo
+        double saldo = scanner.nextDouble();
 
-        // Fechamento do objeto Scanner
+        // Exibição da mensagem de confirmação
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, "
+                        + "sua agência é " + agencia + ", conta " + numero + " e seu saldo " 
+                        + saldo + " já está disponível para saque.";
+
+        System.out.println(mensagem);
+
+        // Fechar o Scanner
         scanner.close();
-
-        // Exibição da mensagem de confirmação usando concatenação de strings
-        String mensagem = String.format(
-                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",
-                nomeCliente, agencia, numero, saldo);
-
-        System.out.println(mensagem); // Exibe a mensagem final
     }
 }
